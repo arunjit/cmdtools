08f7c908d0caab2e19e3b54f6526427d5bb1b4fa
Arunjit Singh <opensrc@ajsd.in>
2015-01-24 22:46:16 +0530

    mkpwd: The pwd prompt

---
+++ mkpwd.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	pwd := os.Args[1]
+
+	home := os.Getenv("HOME")
+	srcin := home + "/Developer/src/"
+	srcout := "~src //"
+	confin := home + "/Developer/conf/"
+	confout := "~conf:"
+
+	if strings.HasPrefix(pwd, srcin) {
+		fmt.Printf(strings.Replace(pwd, srcin, srcout, 1))
+	} else if strings.HasPrefix(pwd, confin) {
+		fmt.Printf(strings.Replace(pwd, confin, confout, 1))
+	} else if strings.HasPrefix(pwd, home) {
+		fmt.Printf(strings.Replace(pwd, home, "~", 1))
+	} else {
+		fmt.Printf(pwd)
+	}
+}
